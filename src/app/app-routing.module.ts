9d763bb48b69640996b0d9cdecdb360f07c398cf
Deepak Bawa <deepak4bawa@gmail.com>
2021-08-11 22:08:29 +0530

    setting up the structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BackendComponent } from "./layout/backend/backend.component";
+import { FrontendComponent } from "./layout/frontend/frontend.component";
+
+import { Err404Component } from "./shared/err404/err404.component";
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        component: BackendComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./module/backend/backend.module').then( module => module.BackendModule )
+            }
+        ]
+    },
+    {
+        path: '',
+        component: FrontendComponent,
+        children: [
+            {
+                path: '',
+                loadChildren: () => import('./module/frontend/frontend.module').then( module => module.FrontendModule )
+            }
+        ]
+    },
+    {
+        path: '**',
+        component: Err404Component
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackendComponent } from './layout/backend/backend.component';
+import { FrontendComponent } from './layout/frontend/frontend.component';
+import { BackendFooterComponent } from './shared/backend/backend-footer/backend-footer.component';
+import { BackendHeaderComponent } from './shared/backend/backend-header/backend-header.component';
+import { FrontendFooterComponent } from './shared/frontend/frontend-footer/frontend-footer.component';
+import { FrontendHeaderComponent } from './shared/frontend/frontend-header/frontend-header.component';
+import { Err404Component } from './shared/err404/err404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackendComponent,
+    FrontendComponent,
+    BackendFooterComponent,
+    BackendHeaderComponent,
+    FrontendFooterComponent,
+    FrontendHeaderComponent,
+    Err404Component
   ],
   imports: [
     BrowserModule,
