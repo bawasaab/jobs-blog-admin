60b1d693655bb09d44f5ceac02531fd9207d9ecf
Deepak Bawa <deepak4bawa@gmail.com>
2021-08-25 00:31:29 +0530

    creating the articles crud design

--- src/app/backend/backend-routing.module.ts
+++ src/app/backend/backend-routing.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticlesListComponent } from './articles/articles-list/articles-list.component';
+import { ArticlesMasterComponent } from './articles/articles-master/articles-master.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersListComponent } from './users/users-list/users-list.component';
+import { UsersMasterComponent } from './users/users-master/users-master.component';
 
 const routes: Routes = [
     {
         path: 'dashboard',
         component: DashboardComponent
-    }
+    },
+    {
+        path: 'articles/master/:articleId',
+        component: ArticlesMasterComponent
+    },
+    {
+        path: 'articles/master',
+        component: ArticlesMasterComponent
+    },
+    {
+        path: 'articles',
+        component: ArticlesListComponent
+    },
+    {
+        path: 'users/master/:articleId',
+        component: UsersMasterComponent
+    },
+    {
+        path: 'users/master',
+        component: UsersMasterComponent
+    },
+    {
+        path: 'users',
+        component: UsersListComponent
+    },
 ];
 
 @NgModule({
